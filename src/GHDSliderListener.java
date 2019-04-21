6a2492ee0c18df00766fe28a16cf92361320fa50
BuildTools <ryan.gamble.819@ou.edu>
2019-04-21 14:22:47 -0500

    Finished GHDSliderListener

--- src/GHDSliderListener.java
+++ src/GHDSliderListener.java
@@ -1,16 +1,25 @@
+import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class GHDSliderListener implements ChangeListener {
 
+	private GHDFrame ghd;
+
 	public GHDSliderListener(GHDFrame ghdFrame) {
-		// TODO Auto-generated constructor stub
+		ghd = ghdFrame;
 	}
 
+	/**
+	 * Tracks changes of the slider
+	 */
 	@Override
 	public void stateChanged(ChangeEvent e) {
-		// TODO Auto-generated method stub
-
+		if (e.getSource() instanceof JSlider) {
+			JSlider js = (JSlider) e.getSource();
+			if (js.getValueIsAdjusting()) {
+				ghd.sliderText.setText(" " + ghd.slider.getValue() + "\t");
+			}
+		}
 	}
-
 }
