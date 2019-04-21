3902d493712dd0eba0b7d52821eaedb78b8569f2
BuildTools <ryan.gamble.819@ou.edu>
2019-04-21 15:00:19 -0500

    DistanceArray Method

--- src/GHDUtilities.java
+++ src/GHDUtilities.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import javax.swing.JComboBox;
 
 public class GHDUtilities {
@@ -17,9 +19,28 @@ public class GHDUtilities {
 		
 	}
 
-	public static int[] calcDistanceArray(String selectedItem) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public static int[] calcDistanceArray(String STID) throws IOException {
+		int[] distanceArray = new int[5];
+		String[] array = new String[GHDFrame.EST_NUM_OF_WORDS];
+		int wordCount = getMesonetStations(array);
 
+		int hDist = 0;
+		for (int index = 0; index < wordCount; index++) {
+			hDist = new Integer(calcDist(STID, array[index]));
+			if (hDist == 0) {
+				distanceArray[0] = distanceArray[0] + 1;
+			} else if (hDist == 1) {
+				distanceArray[1] = distanceArray[1] + 1;
+			} else if (hDist == 2) {
+				distanceArray[2] = distanceArray[2] + 1;
+			} else if (hDist == 3) {
+				distanceArray[3] = distanceArray[3] + 1;
+				;
+			} else if (hDist == 4) {
+				distanceArray[4] = distanceArray[4] + 1;
+				;
+			}
+		}
+		return distanceArray;
+	}
 }
