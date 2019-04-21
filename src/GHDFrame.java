7c26a12e83635ac7b513684ec29c5f79c441ed56
BuildTools <ryan.gamble.819@ou.edu>
2019-04-21 14:14:21 -0500

    Added Fields

--- src/GHDFrame.java
+++ src/GHDFrame.java
@@ -1,4 +1,33 @@
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class GHDFrame {
+	private static final int FRAME_HEIGHT = 1000;
+	private static final int FRAME_WIDTH = 500;
+	protected static final int EST_NUM_OF_WORDS = 500;
 
+	private GHDPanel containerPanel;
+
+	protected JTextField sliderText;
+	protected JSlider slider;
+
+	protected JButton showStation;
+	protected JScrollPane showStationPane;
+	protected JTextArea showStationText;
+
+	protected JComboBox<String> dropMenu;
+	protected JButton calcHD;
+
+	protected JTextField dist0;
+	protected JTextField dist1;
+	protected JTextField dist2;
+	protected JTextField dist3;
+	protected JTextField dist4;
+
+	protected JButton addStation;
+	protected JTextField addName;
 }
