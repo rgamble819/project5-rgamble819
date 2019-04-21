b9f2f4229eab4ab2486ecc3ad41b254e43ac6466
BuildTools <ryan.gamble.819@ou.edu>
2019-04-21 14:25:48 -0500

    Added ButtonListener Functionallity

--- src/GHDButtonListener.java
+++ src/GHDButtonListener.java
@@ -1,16 +1,49 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 
 public class GHDButtonListener implements ActionListener {
 
+	private GHDFrame ghd;
+
+	/**
+	 * 
+	 * @param GHDFrame
+	 *            ghdFrame - The frame the listener corresponds to.
+	 */
 	public GHDButtonListener(GHDFrame ghdFrame) {
-		// TODO Auto-generated constructor stub
+		ghd = ghdFrame;
 	}
 
+	/**
+	 * Performs actions based on what button is pressed on the menu.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if (e.getSource() instanceof JButton) {
+			JButton jb = (JButton) e.getSource();
+			if (jb.getText().equals("Show Station")) {
+				String[] stationsList = new String[GHDFrame.EST_NUM_OF_WORDS];
+				int wordCount = GHDUtilities.getMesonetStations(stationsList);
 
+				ArrayList<String> equalDist = new ArrayList<>();
+				String StID = (String) ghd.dropMenu.getSelectedItem();
+				int distance = ghd.slider.getValue();
+
+				for (int index = 0; index < wordCount; index++) {
+					if (GHDUtilities.calcDist(StID, stationsList[index]) == distance) {
+						equalDist.add(stationsList[index]);
+					}
+				}
+
+				ghd.showStationText.setText("");
+				for (String equals : equalDist) {
+					ghd.showStationText.setText(ghd.showStationText.getText() + "\n" + equals);
+				}
+			}
+		}
 	}
-
 }
--- src/GHDUtilities.java
+++ src/GHDUtilities.java
@@ -1,4 +1,20 @@
+import javax.swing.JComboBox;
 
 public class GHDUtilities {
+
+	public static int getMesonetStations(String[] stationsList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static int calcDist(String stID, String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void refreshMenu(JComboBox<String> dropMenu) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
